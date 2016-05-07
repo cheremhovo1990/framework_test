3a8a2576b765d6c736abe27ee5654175f12604f4
Ivan <cheremhovo1990@yandex.ru>
2016-05-07 09:26:21 +0900

    + phpunit framework

---
+++ phpunit_test/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 07.05.2016
+ * Time: 8:19
+ */
---
+++ phpunit_test/db/MyTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 07.05.2016
+ * Time: 8:21
+ */
+
+class MyTest extends PHPUnit_Framework_TestCase
+{
+    public function testMy()
+    {
+
+    }
+}
