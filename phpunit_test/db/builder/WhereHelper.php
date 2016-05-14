1717995a3cd7e8f458565f515d80b5c4993280df
Ivan <cheremhovo1990@yandex.ru>
2016-05-14 12:05:38 +0900

    + WhereHelper

---
+++ phpunit_test/db/builder/WhereHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 14.05.2016
+ * Time: 10:39
+ */
+
+namespace unit\db\builder;
+
+class WhereHelper extends \PHPUnit_Framework_TestCase
+{
+
+}
--- phpunit_test/db/builder/WhereTest.php
+++ phpunit_test/db/builder/WhereTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-class WhereTest extends PHPUNit_framework_TestCase
+class WhereTest extends unit\db\builder\WhereHelper
 {
     private $where;
 
@@ -10,32 +10,51 @@ class WhereTest extends PHPUNit_framework_TestCase
     {
         $this->where = new app\db\builder\Where();
     }
-    
+
     public function testParser1()
     {
-        $this->where->parser('param');
-        $wheres = $this->where->getWheres();
-        $ands = $wheres->getOperator();
-        $this->assertEquals('param', $ands[0]->getString());
+        /* @var $and \app\db\builder\WhereAnd*/
+        /* @var $string \app\db\builder\SqlString */
+
+        $and = $this->getFromWheres('param', \app\db\builder\WhereAnd::class);
+        $string = $and->getOperator()[0];
+        $this->assertInstanceOf(\app\db\builder\SqlString::class, $string);
+        $this->assertEquals('param', $string->getString());
     }
 
     public function testParser2()
     {
-        $this->where->parser(['or', 'str=param1', ['or', 'str=param2']]);
-        $wheres = $this->where->getWheres();
-        $ors = $wheres->getOperator();
-        $this->assertEquals('str=param1', $ors[0]->getString());
-        $ors = $ors[1]->getOperator();
-        $this->assertEquals('str=param2', $ors[0]->getString());
+        /* @var $or \app\db\builder\WhereOr */
+        /* @var $string \app\db\builder\SqlString */
+
+        $or = $this->getFromWheres(['or', 'str=param1', ['or', 'str=param2']], \app\db\builder\WhereOr::class);
+        $string = $or->getOperator()[0];
+        $this->assertEquals('str=param1', $string->getString());
+        $or = $or->getOperator()[1];
+        $this->assertInstanceOf(\app\db\builder\WhereOr::class, $or);
+        $string = $or->getOperator()[0];
+        $this->assertEquals('str=param2', $string->getString());
     }
 
     public function testParser3()
     {
-        $this->where->parser(['and', 'str=param1', ['and', 'str=param2']]);
+        /* @var $and \app\db\builder\WhereAnd*/
+        /* @var $string \app\db\builder\SqlString*/
+
+        $and = $this->getFromWheres(['and', 'str=param1', ['and', 'str=param2']], \app\db\builder\WhereAnd::class);
+        $string = $and->getOperator()[0];
+        $this->assertEquals('str=param1', $string->getString());
+        $and = $and->getOperator()[1];
+        $this->assertInstanceOf(\app\db\builder\WhereAnd::class, $and);
+        $string = $and->getOperator()[0];
+        $this->assertEquals('str=param2', $string->getString());
+    }
+
+    protected function getFromWheres($param, $class)
+    {
+        $this->where->parser($param);
         $wheres = $this->where->getWheres();
-        $ors = $wheres->getOperator();
-        $this->assertEquals('str=param1', $ors[0]->getString());
-        $ors = $ors[1]->getOperator();
-        $this->assertEquals('str=param2', $ors[0]->getString());
+        $this->assertInstanceOf($class, $wheres);
+        return $wheres;
     }
 }
