1379057ba9980a61ffc6bdceb2a6a0432f853e5b
Ivan <cheremhovo1990@yandex.ru>
2016-05-15 09:47:59 +0900

    + StatementHelper

--- phpunit_test/db/builder/StatementTest.php
+++ phpunit_test/db/builder/StatementTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-class StatementTest extends PHPUNit_framework_TestCase
+class StatementTest extends unit\db\builder\StatementHelper
 {
-    private $statement;
+    protected $statement;
 
     public function setUp()
     {
@@ -13,67 +13,64 @@ class StatementTest extends PHPUNit_framework_TestCase
 
     public function testParser1()
     {
-        $this->statement->parser('select', 'first');
         /* @var $select \app\db\builder\Select */
+
+        $this->statement->parser('select', 'first');
         $select = $this->statement->getStatements()[0];
         $this->assertInstanceOf(\app\db\builder\Select::class, $select);
-        $selects = $select->getTokens();
-        $this->assertEquals('first', $selects[0]->getString());
+        $this->assertSqlStringEquals('first', $select->getTokens()[0]);
     }
 
     public function testParser2()
     {
-        $this->statement->parser('select', ['first', 'as_second' => 'second', 'third as as_third']);
         /* @var $select \app\db\builder\Select */
+
+        $this->statement->parser('select', ['first', 'as_second' => 'second', 'third as as_third']);
         $select = $this->statement->getStatements()[0];
         $this->assertInstanceOf(\app\db\builder\Select::class, $select);
         $selects = $select->getTokens();
-        $this->assertEquals('first', $selects[0]->getString());
-        $this->assertEquals('second AS as_second', $selects[1]->getString());
-        $this->assertEquals('third as as_third', $selects[2]->getString());
+        $this->assertSqlStringEquals('first', $selects[0]);
+        $this->assertSqlStringEquals('second AS as_second', $selects[1]);
+        $this->assertSqlStringEquals('third as as_third', $selects[2]);
     }
 
     public function testParser3()
     {
+        /* @var $from \app\db\builder\From */
+
         $this->statement->parser('from', 'first');
-        /* @var $select \app\db\builder\From */
         $from = $this->statement->getStatements()[0];
         $this->assertInstanceOf(\app\db\builder\From::class, $from);
-        $froms = $from->getTokens();
-        $this->assertEquals('first', $froms[0]->getString());
+        $this->assertSqlStringEquals('first', $from->getTokens()[0]);
     }
 
     public function testParser4()
     {
+        /* @var $from \app\db\builder\From */
+
         $this->statement->parser('from', ['first', 'as_second' => 'second', 'third as as_third']);
-        /* @var $select \app\db\builder\From */
         $from = $this->statement->getStatements()[0];
         $this->assertInstanceOf(\app\db\builder\From::class, $from);
         $froms = $from->getTokens();
-        $this->assertEquals('first', $froms[0]->getString());
-        $this->assertEquals('second AS as_second', $froms[1]->getString());
-        $this->assertEquals('third as as_third', $froms[2]->getString());
+        $this->assertSqlStringEquals('first', $froms[0]);
+        $this->assertSqlStringEquals('second AS as_second', $froms[1]);
+        $this->assertSqlStringEquals('third as as_third', $froms[2]);
     }
 
     public function testParser5()
     {
-        $this->statement->parser('where', ['str=param1', ['or', 'str=param2']]);
         /* @var $where \app\db\builder\Where */
+        /* @var $and \app\db\builder\WhereAnd */
+        /* @var $or \app\db\builder\WhereOr */
+
+        $this->statement->parser('where', ['str=param1', ['or', 'str=param2']]);
         $where = $this->statement->getStatements()[0];
         $this->assertInstanceOf(\app\db\builder\Where::class, $where);
-        /* @var $and \app\db\builder\WhereAnd */
         $and = $where->getWheres();
         $this->assertInstanceOf(\app\db\builder\WhereAnd::class, $and);
-        /* @var $string \app\db\builder\SqlString */
-        $string = $and->getOperator()[0];
-        $this->assertInstanceOf(\app\db\builder\SqlString::class, $string);
-        $this->assertEquals('str=param1', $string->getString());
-        /* @var $or \app\db\builder\WhereOr */
+        $this->assertSqlStringEquals('str=param1', $and->getOperator()[0]);
         $or = $and->getOperator()[1];
-        /* @var $string \app\db\builder\SqlString */
-        $string = $or->getOperator()[0];
-        $this->assertInstanceOf(\app\db\builder\SqlString::class, $string);
-        $this->assertEquals('str=param2', $string->getString());
+        $this->assertInstanceOf(\app\db\builder\WhereOr::class, $or);
+        $this->assertSqlStringEquals('str=param2', $or->getOperator()[0]);
     }
-
 }
